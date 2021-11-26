a48ee1bf69c65f85694cf707657c90ddedaa4985
mankosya <akambarbekova1506@gmail.com>
2021-11-26 23:49:37 +0600

    Добавили формулу регистрации пользователя

--- FirstLesson/app/src/main/java/com/example/firstlesson/MainActivity.kt
+++ FirstLesson/app/src/main/java/com/example/firstlesson/MainActivity.kt
@@ -4,33 +4,43 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var firstName: EditText
-    lateinit var age: EditText
-    lateinit var birthDate: EditText
+   lateinit var editText: EditText
+   lateinit var button: Button
+   lateinit var top: ImageView
+    lateinit var tshirt: ImageView
+    lateinit var blouse: ImageView
 
-    lateinit var saveBtn: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        firstName = findViewById(R.id.first_name)
-        age = findViewById(R.id.ageEditText)
-        birthDate = findViewById(R.id.Date)
-        saveBtn = findViewById(R.id.saveBtn)
+        editText=findViewById(R.id.edittext)
+        top=findViewById(R.id.top)
+        tshirt=findViewById(R.id.tshirt)
+        blouse=findViewById(R.id.blouse)
+        button=findViewById(R.id.button)
 
-        saveBtn.setOnClickListener {
-            val name = firstName.text.toString()
-            val age = age.text.toString().toInt()
-            val dayOfBirth = birthDate.text.toString()
-//            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("NAME", name)
-            intent.putExtra("AGE", age)
-            intent.putExtra("DATE", dayOfBirth)
+        button.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            when{
+                editText.text.toString() == "1" -> intent.putExtra("PICTURE", R.drawable.ic_top)
+                editText.text.toString() == "2" -> intent.putExtra("PICTURE", R.drawable.ic_tshirt)
+                editText.text.toString() == "3" -> intent.putExtra("PICTURE", R.drawable.ic_blouse)
+                else -> {
+                    Toast.makeText(this, "Введите цифру из предложенных!", Toast.LENGTH_SHORT).show()
+                }
+            }
             startActivity(intent)
         }
+
+        }
     }
-}
+
+
---
+++ FirstLesson/app/src/main/java/com/example/firstlesson/MainActivity2.kt
@@ -0,0 +1,19 @@
+package com.example.firstlesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        imageView = findViewById(R.id.imageView)
+
+        val image = intent.getIntExtra("PICTURE", 0)
+        imageView.setImageResource(image)
+    }
+}
